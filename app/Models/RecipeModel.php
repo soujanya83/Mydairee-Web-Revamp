bf844d6743b24e79b91be476e096ba5f6a1bce24
Adil-Developer1 <developer1@fraxxratech.com>
2026-05-09 11:29:30 +0530

    Ingridient List

--- app/Models/RecipeModel.php
+++ app/Models/RecipeModel.php
@@ -10,4 +10,25 @@ class RecipeModel extends Model
     public $timestamps = false;
 
     protected $fillable = ['itemName', 'type', 'recipe', 'centerid', 'createdBy','foodtype','notes','RecipeVideolink'];
+
+    /**
+     * Get ingredients associated with this recipe
+     */
+    public function ingredients()
+    {
+        return $this->belongsToMany(
+            IngredientModel::class,
+            'recipe_ingredients',
+            'recipeId',
+            'ingredientId'
+        )->select('ingredients.id', 'ingredients.name');
+    }
+
+    /**
+     * Get media (images/videos) associated with this recipe
+     */
+    public function media()
+    {
+        return $this->hasMany(RecipeMediaModel::class, 'recipeid', 'id');
+    }
 }
